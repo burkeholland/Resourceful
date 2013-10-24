532a080465d0add330ce8f68d8aba69e786441f7
Burke Holland <burkeholland@gmail.com>
2013-10-24 17:29:35 -0500

    Reservations controller returns View and handles scheduler

---
+++ Controllers/ReservationsController.cs
@@ -0,0 +1,35 @@
+﻿using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Resourceful.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        readonly Repositories.ScheduleRepository _scheduleRepo = new Repositories.ScheduleRepository();
+
+        public ActionResult Index()
+        {
+            ViewBag.Tab = "reservations";
+
+            return View();
+        }
+
+        public ActionResult Get([DataSourceRequest] DataSourceRequest request) {
+
+            var schedules = _scheduleRepo.All();
+
+            var reservations = new List<Models.Reservation>();
+
+            foreach (var schedule in schedules) {
+                reservations.AddRange(Models.Map.Reservation(schedule));
+            }
+
+            return Json(reservations.ToDataSourceResult(request));
+        }
+    }
+}
