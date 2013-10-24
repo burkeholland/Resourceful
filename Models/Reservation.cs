e31c21352ad9645782b97de6fe85c8046c33fa37
Burke Holland <burkeholland@gmail.com>
2013-10-24 17:29:55 -0500

    Reservation model implementing ISchedulerEvent

---
+++ Models/Reservation.cs
@@ -0,0 +1,19 @@
+﻿using Kendo.Mvc.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Resourceful.Models {
+    public class Reservation : ISchedulerEvent {
+        public string Description { get; set; }
+        public DateTime End { get; set; }
+        public bool IsAllDay { get; set; }
+        public string RecurrenceException { get; set; }
+        public string RecurrenceRule { get; set; }
+        public DateTime Start { get; set; }
+        public string Title { get; set; }
+        public int ResourceID { get; set; }
+        public int ReservationID { get; set; }
+    }
+}
